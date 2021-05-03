ac0635c8e7b5504835ddac424500efab2a0ddb9b
JeongHwi <wjdgnl97@gmail.com>
2021-05-03 14:16:22 +0900

    [Mediapipe] 832 update , Angle Update

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,9 +1,37 @@
+/*
+ * Copyright 2020 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.myapplication;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.util.Log;
 import android.util.Size;
+import android.view.PixelCopy;
+import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -17,6 +45,7 @@ import com.google.mediapipe.components.PermissionHelper;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmarkList;
 import com.google.mediapipe.formats.proto.LandmarkProto.NormalizedLandmark;
 import com.google.mediapipe.framework.AndroidAssetUtil;
+import com.google.mediapipe.framework.AndroidPacketGetter;
 import com.google.mediapipe.framework.Packet;
 import com.google.mediapipe.framework.PacketGetter;
 import com.google.mediapipe.glutil.EglManager;
@@ -27,6 +56,12 @@ import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.graphics.SurfaceTexture;
 import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.lang.Math;
+
+
+
 /**
  * Main activity of MediaPipe example apps.
  */
@@ -75,15 +110,13 @@ public class MainActivity extends AppCompatActivity {
         previewDisplayView = new SurfaceView(this);
         setupPreviewDisplayView();
 
+
         try {
             applicationInfo =
                     getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
         } catch (NameNotFoundException e) {
             Log.e(TAG, "Cannot find application info: " + e);
         }
-
-
-
         // Initialize asset manager so that MediaPipe native libraries can access the app assets, e.g.,
         // binary graphs.
         AndroidAssetUtil.initializeNativeAssetManager(this);
@@ -91,34 +124,57 @@ public class MainActivity extends AppCompatActivity {
         processor =
                 new FrameProcessor(
                         this,
-                                eglManager.getNativeContext(),
-                                BINARY_GRAPH_NAME,
-                                INPUT_VIDEO_STREAM_NAME,
-                                OUTPUT_VIDEO_STREAM_NAME);
+                        eglManager.getNativeContext(),
+                        BINARY_GRAPH_NAME,
+                        INPUT_VIDEO_STREAM_NAME,
+                        OUTPUT_VIDEO_STREAM_NAME);
         processor
                 .getVideoSurfaceOutput()
                 .setFlipY(FLIP_FRAMES_VERTICALLY);
-
 
 
         // To show verbose logging, run:
         // adb shell setprop log.tag.MainActivity VERBOSE
 //        if (Log.isLoggable(TAG, Log.VERBOSE)) {
         processor.addPacketCallback(
-            OUTPUT_LANDMARKS_STREAM_NAME,
-            (packet) -> {
-                Log.v(TAG, "Received Pose landmarks packet.");
-                try {
+                OUTPUT_LANDMARKS_STREAM_NAME,
+                (packet) -> {
+                    Log.v(TAG, "Received Pose landmarks packet.");
+                    try {
 //                        NormalizedLandmarkList poseLandmarks = PacketGetter.getProto(packet, NormalizedLandmarkList.class);
-                    byte[] landmarksRaw = PacketGetter.getProtoBytes(packet);
-                    NormalizedLandmarkList poseLandmarks = NormalizedLandmarkList.parseFrom(landmarksRaw);
-                    Log.v(TAG,"[TS:"+ packet.getTimestamp()+ "] "+ getPoseLandmarksDebugString(poseLandmarks));
-                } catch(InvalidProtocolBufferException exception){
-                    Log.e(TAG, "failed to get proto.",exception);
+                        byte[] landmarksRaw = PacketGetter.getProtoBytes(packet);
+                        NormalizedLandmarkList poseLandmarks = NormalizedLandmarkList.parseFrom(landmarksRaw);
+                        Log.v(TAG, "[TS:" + packet.getTimestamp() + "] " + getPoseLandmarksDebugString(poseLandmarks));
+                        SurfaceHolder srh = previewDisplayView.getHolder();
+//
+//                  -- this line cannot Running --
+//                    Canvas canvas = null;
+//                    try {
+//                        canvas= srh.lockCanvas();
+//                        synchronized(srh){
+//                            Paint paint = new Paint();
+//                            paint.setColor(Color.RED);
+//                            canvas.drawCircle(10.0f,10.0f,10.0f,paint);
+//                        }
+//                    }finally{
+//                        if(canvas != null){
+//                            srh.unlockCanvasAndPost(canvas);
+//                        }
+//                    }
+////                    processor.getVideoSurfaceOutput().setSurface(srh.getSurface());
+                    } catch (InvalidProtocolBufferException exception) {
+                        Log.e(TAG, "failed to get proto.", exception);
+                    }
+
                 }
-            }
         );
-//        }
+        /*processor.addPacketCallback(
+                "throttled_input_video_cpu",
+                (packet) ->{
+                    Log.d("Raw Image","Receive image with ts: "+packet.getTimestamp());
+                    Bitmap image = AndroidPacketGetter.getBitmapFromRgba(packet);
+                }
+        );*/
         PermissionHelper.checkAndRequestCameraPermissions(this);
     }
 
@@ -159,6 +215,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCameraStarted(SurfaceTexture surfaceTexture) {
         previewFrameTexture = surfaceTexture;
+
         // Make the display view visible to start showing the preview. This triggers the
         // SurfaceHolder.Callback added to (the holder of) previewDisplayView.
         previewDisplayView.setVisibility(View.VISIBLE);
@@ -172,8 +229,8 @@ public class MainActivity extends AppCompatActivity {
         cameraHelper = new CameraXPreviewHelper();
         cameraHelper.setOnCameraStartedListener(
                 surfaceTexture -> {
-        onCameraStarted(surfaceTexture);
-    });
+                    onCameraStarted(surfaceTexture);
+                });
         CameraHelper.CameraFacing cameraFacing = CameraHelper.CameraFacing.BACK;
         cameraHelper.startCamera(
                 this, cameraFacing, /*unusedSurfaceTexture=*/ null, cameraTargetResolution());
@@ -192,13 +249,17 @@ public class MainActivity extends AppCompatActivity {
         Size displaySize = cameraHelper.computeDisplaySizeFromViewSize(viewSize);
         boolean isCameraRotated = cameraHelper.isCameraRotated();
 
+        //displaySize.getHeight(); 핸드폰 디스플레이 사이즈를 의미
+        //displaySize.getWidth();
+
+
         // Connect the converter to the camera-preview frames as its input (via
         // previewFrameTexture), and configure the output width and height as the computed
         // display size.
         converter.setSurfaceTextureAndAttachToGLContext(
                 previewFrameTexture,
                 isCameraRotated ? displaySize.getHeight() : displaySize.getWidth(),
-        isCameraRotated ? displaySize.getWidth() : displaySize.getHeight());
+                isCameraRotated ? displaySize.getWidth() : displaySize.getHeight());
     }
 
     private void setupPreviewDisplayView() {
@@ -213,27 +274,71 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void surfaceCreated(SurfaceHolder holder) {
                                 processor.getVideoSurfaceOutput().setSurface(holder.getSurface());
+                                Log.d("Surface","Surface Created");
+
                             }
 
                             @Override
                             public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
                                 onPreviewDisplaySurfaceChanged(holder, format, width, height);
+                                // 이곳에서 width , height 가 720,1280
+                                Log.d("Surface","Surface Changed");
                             }
 
                             @Override
                             public void surfaceDestroyed(SurfaceHolder holder) {
                                 processor.getVideoSurfaceOutput().setSurface(null);
+                                Log.d("Surface","Surface destroy");
                             }
+
                         });
+
     }
-    //여기 확률
+    //해당 코드에서 landmark의 좌표를 추출해낼 수 있다.
+    //[0.0 , 1.0] 으로 normazlized 된 coordinate -> image width, height
     private static String getPoseLandmarksDebugString(NormalizedLandmarkList poseLandmarks) {
         String poseLandmarkStr = "Pose landmarks: " + poseLandmarks.getLandmarkCount() + "\n";
+        ArrayList<PoseLandMark> poseMarkers= new ArrayList<PoseLandMark>();
         int landmarkIndex = 0;
         for (NormalizedLandmark landmark : poseLandmarks.getLandmarkList()) {
-            poseLandmarkStr += "\tLandmark ["+ landmarkIndex+ "]: ("+ landmark.getX()+ ", "+ landmark.getY()+ ", "+ landmark.getZ()+ ")\n";
+            PoseLandMark marker = new PoseLandMark(landmark.getX(),landmark.getY(),landmark.getVisibility());
+//          poseLandmarkStr += "\tLandmark ["+ landmarkIndex+ "]: ("+ (landmark.getX()*720)+ ", "+ (landmark.getY()*1280)+ ", "+ landmark.getVisibility()+ ")\n";
             ++landmarkIndex;
+            poseMarkers.add(marker);
         }
+        // Get Angle of Positions
+        double rightAngle = getAngle(poseMarkers.get(16),poseMarkers.get(14),poseMarkers.get(12));
+        double leftAngle = getAngle(poseMarkers.get(15),poseMarkers.get(13),poseMarkers.get(11));
+        double rightKnee = getAngle(poseMarkers.get(24),poseMarkers.get(26),poseMarkers.get(28));
+        double leftKnee = getAngle(poseMarkers.get(23),poseMarkers.get(25),poseMarkers.get(27));
+        double rightShoulder = getAngle(poseMarkers.get(14),poseMarkers.get(12),poseMarkers.get(24));
+        double leftShoulder = getAngle(poseMarkers.get(13),poseMarkers.get(11),poseMarkers.get(23));
+        Log.v(TAG,"======Degree Of Position]======\n"+
+                "rightAngle :"+rightAngle+"\n"+
+                "leftAngle :"+leftAngle+"\n"+
+                "rightHip :"+rightKnee+"\n"+
+                "leftHip :"+leftKnee+"\n"+
+                "rightShoulder :"+rightShoulder+"\n"+
+                "leftShoulder :"+leftShoulder+"\n");
         return poseLandmarkStr;
+        /*
+           16 오른 손목 14 오른 팔꿈치 12 오른 어깨 --> 오른팔 각도
+           15 왼쪽 손목 13 왼쪽 팔꿈치 11 왼쪽 어깨 --> 왼  팔 각도
+           24 오른 골반 26 오른 무릎   28 오른 발목 --> 오른무릎 각도
+           23 왼쪽 골반 25 왼쪽 무릎   27 왼쪽 발목 --> 왼 무릎 각도
+           14 오른 팔꿈 12 오른 어깨   24 오른 골반 --> 오른 겨드랑이 각도
+           13 왼   팔꿈 11 왼  어깨   23  왼  골반 --> 왼쪽 겨드랑이 각도
+        */
+    }
+    static double getAngle(PoseLandMark firstPoint, PoseLandMark midPoint, PoseLandMark lastPoint) {
+        double result =
+                Math.toDegrees(
+                        Math.atan2(lastPoint.getY() - midPoint.getY(),lastPoint.getX() - midPoint.getX())
+                                - Math.atan2(firstPoint.getY() - midPoint.getY(),firstPoint.getX() - midPoint.getX()));
+        result = Math.abs(result); // Angle should never be negative
+        if (result > 180) {
+            result = (360.0 - result); // Always get the acute representation of the angle
+        }
+        return result;
     }
 }
